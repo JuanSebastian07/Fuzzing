fe6ba50e05c6ede4de521309176e591c07dbc8ec
JuanSebastian07 <93341765+JuanSebastian07@users.noreply.github.com>
2022-04-18 22:20:04 -0500

    Update fuzzing.py

--- fuzzing.py
+++ fuzzing.py
@@ -15,7 +15,7 @@ def requestGet(response):
         #Quitamos 'url="/wp-plugins/'
         a=re.sub('url="/wp-plugins',"",match.group(0))
         #Aqui pones el link al que deseas hacerle fuzzing con los plugins wp
-        b = f'{"https://www.google.com/wp-content/plugins"}{a}'
+        b = f'https://www.google.com/wp-content/plugins{a}'
         response_uri = requests.get(b)
 
         if(response_uri.status_code != 404):
@@ -29,6 +29,6 @@ def cambiar_pagina():
     for i in range(1,1000):
         requestGet(requests.get(f'{"https://github.com/orgs/wp-plugins/repositories?page="}{i}'))
         #print(lista)
-        print(f'{"------------Pag"}{i}---------------')
+        print(f'-----------pag{i}--------------')
     
 cambiar_pagina()
